d45ef770e45060f00d455261b56473a184c8940b
owenliang <owenliang1990@gmail.com>
2018-10-18 17:18:34 +0800

    update

--- models/__init__.py
+++ models/__init__.py
@@ -3,4 +3,4 @@
 import web
 
 # 数据库
-db_webpy = web.database(dbn = 'mysql', user = 'root', pw='baidu@123', db='webpy', pooling = False,)   # 线程安全, mysql连接随着线程退出销毁(threadlocal)
+db_webpy = web.database(dbn = 'mysql', user = 'root', pw='baidu@123', db='webpy', pooling = False,)   # 单连接, 每次请求结束会被立即释放
