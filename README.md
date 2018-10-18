71ae77e8281b9c52c9547796eb620825868acbee
owenliang <owenliang1990@gmail.com>
2018-10-18 17:17:40 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # webpy-demo
+
 从源码中来，到应用中去，靠谱的webpy用法
+
+# 依赖
+
+* pip3 install git+https://github.com/webpy/webpy#egg=web.py
+* pip3 install PyMySQL
+* pip3 install gunicorn
+
+# 启动
+
+gunicorn -w 4 app:app
--- app.py
+++ app.py
@@ -6,7 +6,10 @@ urls = (
     '/', 'controllers.index.index'  # 只有这样传完整包名, web-py才能帮我们自动reload修改后的handler.index module
 )
 
+# web.config.debug = False # 生产模式
+app = web.application(urls, globals())
+
 if __name__ == "__main__":
-    # web.config.debug = False # 生产模式
-    app = web.application(urls, globals())
     app.run()
+else:
+    app = app.wsgifunc()
