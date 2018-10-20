e41ec1a11646d36c046770dde0b577e14d7c6e6f
owenliang1990 <owenliang1990@gmail.com>
2018-10-20 19:35:31 +0800

    update

--- app.py
+++ app.py
@@ -2,9 +2,11 @@
 
 import web
 import models
+from web.httpserver import StaticMiddleware
 
 urls = (
-    '/', 'controllers.index.index'  # 只有这样传完整包名, web-py才能帮我们自动reload修改后的handler.index module
+    '/', 'controllers.index.index',  # 只有这样传完整包名, web-py才能帮我们自动reload修改后的handler.index module
+    '/layui_demo', 'controllers.layui.layui'
 )
 
 # web.config.debug = False # 生产模式
@@ -13,4 +15,5 @@ app = web.application(urls, globals())
 if __name__ == "__main__":
     app.run()
 else:
-    app = app.wsgifunc()
+    # 传入中间件class
+    app = app.wsgifunc(StaticMiddleware)
---
+++ controllers/layui.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import views
+
+# 控制器
+class layui:
+    # GET请求
+    def GET(self):
+        return views.render.layui()
