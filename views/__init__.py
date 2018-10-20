4c4a2bf5a372284f38d6d47d065cb8c121662c82
owenliang <owenliang1990@gmail.com>
2018-10-20 20:11:07 +0800

    update

--- views/__init__.py
+++ views/__init__.py
@@ -3,6 +3,6 @@
 import web
 
 # 用于渲染HTML模板
-render = web.template.render('views/', )
+render = web.template.render('views/', cache = False)   # 禁用模板缓存特性
 # 给模板渲染环境增加全局变量, 这样模板之间就可以嵌套了
 render._add_global(render, 'render')
